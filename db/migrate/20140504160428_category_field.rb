2244b340bdfb5c75517082120cf7bfde1e2d2763
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2014-05-04 12:08:28 -0400

    Migrations

--- db/migrate/20140504160428_category_field.rb
+++ db/migrate/20140504160428_category_field.rb
@@ -2,7 +2,9 @@ class CategoryField < ActiveRecord::Migration
   def change
     change_table :pages do |t|
       t.remove :category
+      t.remove :picture
       t.text :category
+      t.text :picture
     end
   end
 end
